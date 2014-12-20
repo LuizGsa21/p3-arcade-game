0b5030a7462eaa00caafec33ec25096c4b8113ef
LuizGsa21 <LArantesSa@icloud.com>
2014-12-20 16:19:54 -0600

    Add player class template

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var Moveable = function(rowPosition, colPosition, speed) {
     this.x = 0 + (colPosition * 101);
     this.y = (rowPosition * 83) - 41.5;
     this.speed = speed;
-}
+};
 // Enemies our player must avoid
 var Enemy = function(rowPosition, colPosition, speed) {
     Moveable.call(rowPosition, colPosition, speed);
@@ -20,30 +20,50 @@ var Enemy = function(rowPosition, colPosition, speed) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+};
 
-// Update the enemy's position, required method for game
+// Update the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-}
+};
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(rowPosition, colPosition, speed) {
+    Moveable.call(rowPosition, colPosition, speed);
 
+    this.sprite = 'images/char-boy.png';
+};
+
+// Update the player's position
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+
+};
+
+// Draw the player on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Called when player presses an arrow key
+Player.prototype.handleInput = function(keyCode) {
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var player = new Player(5, 2, 1);
 
 
 // This listens for key presses and sends the keys to your
