71138a15b61835fb36e7e596326d8424516d0f59
LuizGsa21 <LArantesSa@icloud.com>
2014-12-21 14:20:23 -0600

    Refactor code 	Add a Const object for easier access of game objects. 	Move render method in Enemy and Player to Moveable class.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
+// Constant object used for convenience
+var Const = {
+    player: {
+        BOY : "images/char-boy.png",
+        CAT_GIRL : "images/char-cat-girl.png",
+        HORN_GIRL : "char-horn-girl.png",
+        PINK_GIRL : "images/char-pink-girl.png",
+        PRINCESS_GIRL : "images/char-princess-girl.png"
+    },
+    enemy : {
+        "BUG" : "images/enemy-bug.png"
+    },
+    gem : {
+        BLUE : "images/Gem-Blue.png",
+        GREEN : "images/Gem-Green.png",
+        ORANGE : "images/Gem-Orange.png"
+    },
+    misc : {
+        KEY: "images/Key.png",
+        STAR: "images/Star.png",
+        SELECTOR: "images/Selector.png"
+    },
+    grid : {
+        WIDTH: 101,
+        HEIGHT: 83
+    },
+    canvas: {
+        WIDTH: 505,
+        HEIGHT: 606
+    },
+    SPEED_FACTOR: 100
+};
+
 /**
  * Creates a moveable object.
  * All moveable objects should implement this class.
@@ -7,20 +40,27 @@
  *  widthBounds - imaginary rectangle width, used to detect collision
  *  heightBounds - imaginary rectangle height, used to detect collision
  */
-var Moveable = function(rowPosition, colPosition, widthBounds, heightBounds) {
-    this.x = colPosition * 101;
-    this.y = rowPosition * 83 - 41.5;
+var Moveable = function(rowPosition, colPosition, image, widthBounds, heightBounds) {
+    this.x = colPosition * Const.grid.WIDTH;
+    this.y = (rowPosition * Const.grid.HEIGHT) - 41.5; // subtracting 41.5 to center object on grid
+
+    // The image/sprite will be drawn on screen when render() is called
+    this.sprite = image;
 
     // set default width/height bounds if not specified
-    // Why not use the images default width and height?
-    //  Because the collisions seem to happen to early.
+    // Why not use the grid's default width?
+    //  Because the collisions seem to happen too early.
     this.widthBounds = widthBounds || 80;
-    this.heightBounds = heightBounds || 83;
+    this.heightBounds = heightBounds || Const.grid.HEIGHT;
+};
+
+// Draws the object on screen
+Moveable.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Checks to see if current object collides with given object
 Moveable.prototype.intersects = function (obj) {
-
     // Treat both objects as a rectangle.
     // A collision happens when the two rectangles intersect
     return (this.y < obj.y + obj.heightBounds  &&
@@ -30,18 +70,19 @@ Moveable.prototype.intersects = function (obj) {
 }
 
 // Enemies our player must avoid
-var Enemy = function(rowPosition, colPosition, speed) {
-    Moveable.call(this, rowPosition, colPosition);
-    this.speed = speed; //  Enemy's speed is multiplied by deltaTime * 100
+var Enemy = function(rowPosition, colPosition, image, speed) {
+    Moveable.call(this, rowPosition, colPosition, image);
+    this.speed = speed; //  Enemy's speed is multiplied by deltaTime * SPEED_FACTOR
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = image;
 };
 
 // Enemy extends Moveable class
 Enemy.prototype = Object.create(Moveable.prototype);
-// Recover lost constructor
+
+// Recover Enemy's constructor
 Enemy.prototype.constructor = Enemy;
 
 // Update the enemy's position
@@ -50,38 +91,30 @@ Enemy.prototype.update = function(dt) {
     if (this.x > ctx.canvas.width) {
         this.x = 0;
     } else {
-        this.x += dt * 100 * this.speed;
+        this.x += dt * Const.SPEED_FACTOR * this.speed;
     }
 };
 
-// Draw the enemy on the screen
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(rowPosition, colPosition) {
-    Moveable.call(this, rowPosition, colPosition);
+// Player Class
+var Player = function(rowPosition, colPosition, image) {
+    Moveable.call(this, rowPosition, colPosition, image);
 
     // startX and startY will be the player's respawn position
     this.startX = this.x;
     this.startY = this.y;
-    this.sprite = 'images/char-boy.png';
 };
 
 // Player extends Moveable class
 Player.prototype = Object.create(Moveable.prototype);
-// Recover lost constructor
+
+// Recover Player's constructor
 Player.prototype.constructor = Player;
 
-// Kills player IN REAL LIFE! lolol jk jk
 // resets player to starting position
 Player.prototype.die = function() {
     this.x = this.startX;
     this.y = this.startY;
-}
+};
 
 // Checks if player intersects enemy
 Player.prototype.update = function() {
@@ -93,43 +126,38 @@ Player.prototype.update = function() {
     }
 };
 
-// Draw the player on the screen
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
 // Gets called when player releases a valid key
 Player.prototype.handleInput = function(keyCode) {
 
     switch (keyCode) {
         case 'left':
             if (this.x > 0)
-                this.x -= 101;
+                this.x -= Const.grid.WIDTH;
             break;
         case 'right':
-            if (this.x < ctx.canvas.width - 101)
-                this.x += 101;
+            if (this.x < ctx.canvas.width - Const.grid.WIDTH)
+                this.x += Const.grid.WIDTH;
             break;
         case 'up':
-            if (this.y > 83)
-                this.y -= 83;
+            if (this.y > Const.grid.HEIGHT)
+                this.y -= Const.grid.HEIGHT;
             break;
         case 'down':
             if (this.y < ctx.canvas.height - 303)
-                this.y += 83;
+                this.y += Const.grid.HEIGHT;
             break;
     }
 
 };
 
 // Create player at Row: 5 Col: 2
-var player = new Player(5, 2);
+var player = new Player(5, 2, Const.player.BOY);
 
 // Create Enemies
 var allEnemies = [
-    new Enemy(1, 0, 2),
-    new Enemy(2, 0, 2),
-    new Enemy(3, 0, 2)
+    new Enemy(1, 0, Const.enemy.BUG, 2),
+    new Enemy(2, 0, Const.enemy.BUG, 2),
+    new Enemy(3, 0, Const.enemy.BUG, 2)
 ];
 
 
